c4d4d7302c962d42823069cca8cd7ce02fce233c
Nishashoukhath <nidhashoukhathkp@gmail.com>
2022-08-24 06:17:35 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,42 +14,95 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color.fromARGB(249, 31, 177, 145),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
             CircleAvatar(
               backgroundImage: AssetImage('assets/images/image.png'),
               maxRadius: 50,
             ),
             SizedBox(height: 10),
-            Text('Nidha Shoukhath KP',
-            style: TextStyle(fontFamily: 'Pacifico', color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+            Text(
+              'Nidha Shoukhath KP',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             Text('FLUTTER DEVELOPER',
-            style: TextStyle(color: Colors.white70,fontSize: 15, fontWeight: FontWeight.w500)),
-            Divider(color: Colors.white70, indent: 150,endIndent: 150,thickness: 1),            
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500)),
+            Divider(
+                color: Colors.white70,
+                indent: 150,
+                endIndent: 150,
+                thickness: 1),
             SizedBox(height: 10),
-            Container(width: 350,height: 50,
-            decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5)), color: Colors.white),            
-            child: Row(children: [
-              SizedBox(width: 15,),
-              Icon(Icons.call, color: Color.fromARGB(249, 31, 177, 145),),
-              SizedBox(width: 50,),
-              Text('+91 95 4423 8731',style: TextStyle(color:  Color.fromARGB(249, 31, 177, 145)),)
-            ],),),
-            SizedBox(height: 15,),
-            Container(width: 350,height: 50,
-            decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5)), color: Colors.white),  
-            child: Row(children: [
-              SizedBox(width: 15,),
-              Icon(Icons.mail, color: Color.fromARGB(249, 31, 177, 145),),
-              SizedBox(width: 50,),
-              Text('nidhashoukhathkp@gmail',style: TextStyle(color:  Color.fromARGB(249, 31, 177, 145)),)
-            ],),
-            )            
-          ],)
-        ),
+            GestureDetector(
+              onTap: () => launchUrlString('tel:9544238731'),
+              child: Container(
+                width: 350,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: Colors.white),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.call,
+                      color: Color.fromARGB(249, 31, 177, 145),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      '+91 95 4423 8731',
+                      style: TextStyle(color: Color.fromARGB(249, 31, 177, 145)),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            GestureDetector(
+              onTap: () => launchUrlString('mailto:nidhashoukhathkp@gmail.com'),
+              child: Container(
+                width: 350,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: Colors.white),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.mail,
+                      color: Color.fromARGB(249, 31, 177, 145),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      'nidhashoukhathkp@gmail',
+                      style: TextStyle(color: Color.fromARGB(249, 31, 177, 145)),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
