914b5fad49a61e9acbcdd6952bc4db9c12df5bea
Nishashoukhath <nidhashoukhathkp@gmail.com>
2022-09-07 08:09:11 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color.fromARGB(249, 31, 177, 145),
         body: Center(
